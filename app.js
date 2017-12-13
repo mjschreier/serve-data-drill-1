6f8856440eb3e998f6ea1b330a80ba51375c1ef9
Michael Schreier <mjschreier1@gmail.com>
2017-12-13 16:10:04 -0700

    completed first server drill

---
+++ app.js
@@ -0,0 +1,54 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+app.use(cors());
+
+var cohorts = [{
+    id: 1,
+    cohortName: "17-01-WD-DP",
+    cohortCode: "g100",
+    students: 28,
+  },{
+    id: 2,
+    cohortName: "17-01-DS-GT",
+    cohortCode: "g105",
+    students: 24,
+  },{
+    id: 3,
+    cohortName: "17-02-WD-PX",
+    cohortCode: "g109",
+    students: 29,
+  },{
+    id: 4,
+    cohortName: "17-03-WD-BD",
+    cohortCode: "g110",
+    students: 29,
+  }
+];
+
+function findById(data, id) {
+  for(let i = 0; i < data.length; i++) {
+    if(data[i].id == id) {
+      return data[i];
+    }
+  }
+  return null;
+}
+
+app.get("/", function(request, response) {
+  response.json({data: cohorts});
+})
+
+app.get("/:id", function(request, response) {
+  var record = findById(cohorts, request.params.id);
+  if(!record) {
+    response.status = 404;
+    response.json({
+      error: {
+        message: "No record found",
+    }})
+  }
+  response.json({data: record});
+})
+
+app.listen(3000);
